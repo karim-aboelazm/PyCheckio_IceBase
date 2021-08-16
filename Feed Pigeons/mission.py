2fcdb9cc7da7b0d156cb8be47a21fcdbd1885a09
KARIM ABOELAZM <74789405+karim-aboelazm@users.noreply.github.com>
2021-08-17 01:59:35 +0200

    first commit

---
+++ Feed Pigeons/mission.py
@@ -0,0 +1,9 @@
+def checkio(number):
+    return 0
+
+if __name__ == '__main__':
+    #These "asserts" using only for self-checking and not necessary for auto-testing
+    assert checkio(1) == 1, "1st example"
+    assert checkio(2) == 1, "2nd example"
+    assert checkio(5) == 3, "3rd example"
+    assert checkio(10) == 6, "4th example"
